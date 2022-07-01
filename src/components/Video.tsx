701fd57fd0ecd0b4adf987abe338c2f65a224c0b
Felipe Melo <felipefrmelo@hotmail.com>
2022-07-01 19:45:16 -0300

    optional teacher

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -43,7 +43,7 @@ export function Video({ lessonSlug }: VideoProps) {
     }
   })
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>
@@ -66,24 +66,26 @@ export function Video({ lessonSlug }: VideoProps) {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-            {data.lesson.title}
+              {data.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-            {data.lesson.description}
+              {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                className="h-16 w-16 rounded-full border-2 border-blie-500"
-                src={data.lesson.teacher.avatarURL}
-                alt="Professor"
-              />
+            {data.lesson.teacher &&
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-blie-500"
+                  src={data.lesson.teacher.avatarURL}
+                  alt="Professor"
+                />
 
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                  <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                </div>
               </div>
-            </div>
+            }
           </div>
 
           <div className="flex flex-col gap-4">
